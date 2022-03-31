600e8f3f1bc450804124b0d5165a8e3a901e480c
LKashi <850593913@qq.com>
2022-03-31 16:49:42 +0800

    冒泡排序_v1.1.0

--- tests/bubbleSort.spec.js
+++ tests/bubbleSort.spec.js
@@ -10,4 +10,9 @@ describe("冒泡排序", () => {
     const nums = [5, 1, 1, 2, 0, 0];
     expect(bubbleSort(nums)).toEqual([0, 0, 1, 1, 2, 5]);
   });
+
+  test("[1,2,3]", () => {
+    const nums = [1, 2, 3];
+    expect(bubbleSort(nums)).toEqual([1, 2, 3]);
+  });
 });
--- 冒泡排序/index.js
+++ 冒泡排序/index.js
@@ -2,6 +2,8 @@
 function sortArray(n) {
   // 数组有多长就有多少轮比较
   for (let i = 0; i < n.length; i++) {
+    // 该轮比较是否出现排序
+    let isSorted = false;
     // 每轮比较得到一个新的有序区
     // 下轮比较不需要再对有序区进行比较
     for (let j = 0; j < n.length - 1 - i; j++) {
@@ -11,7 +13,14 @@ function sortArray(n) {
         n[j + 1] = n[j];
         n[j] = temp;
         // 或者使用ES6的解构赋值交换值 [n[i], n[j]] = [n[j], n[i]]
+
+        // 出现排序
+        isSorted = true;
       }
+    }
+    // 该轮比较未出现排序，证明该数组已经有序，不再需要下轮比较
+    if (!isSorted) {
+      break;
     }
   }
 
