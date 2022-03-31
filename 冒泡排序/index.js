d26b0960ad453c78236c46803ad64f744f97bb36
LKashi <850593913@qq.com>
2022-03-31 17:30:47 +0800

    冒泡排序_v1.2.0

--- 冒泡排序/index.js
+++ 冒泡排序/index.js
@@ -1,12 +1,14 @@
 // 冒泡排序
 function sortArray(n) {
-  // 数组有多长就有多少轮比较
-  for (let i = 0; i < n.length; i++) {
-    // 该轮比较是否出现排序
-    let isSorted = false;
+  // 理想状态下，数组有多长就有多少轮比较
+  // 实际情况是，数组有序区长度可能大于比较轮数，导致后面多次比较毫无意义
+  let i = n.length - 1;
+  while (i > 0) {
+    // 最后一次交换的位置
+    let lastChangeIndex = 0;
     // 每轮比较得到一个新的有序区
     // 下轮比较不需要再对有序区进行比较
-    for (let j = 0; j < n.length - 1 - i; j++) {
+    for (let j = 0; j < i; j++) {
       // 如果前一个元素大于后一个元素则交换位置
       if (n[j] > n[j + 1]) {
         let temp = n[j + 1];
@@ -14,14 +16,11 @@ function sortArray(n) {
         n[j] = temp;
         // 或者使用ES6的解构赋值交换值 [n[i], n[j]] = [n[j], n[i]]
 
-        // 出现排序
-        isSorted = true;
+        // 最后一次交换位置作为有序区的边界
+        lastChangeIndex = j;
       }
     }
-    // 该轮比较未出现排序，证明该数组已经有序，不再需要下轮比较
-    if (!isSorted) {
-      break;
-    }
+    i = lastChangeIndex;
   }
 
   return n;
