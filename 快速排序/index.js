8874243275bc18c31bfefef4fd953fd5de489717
LKashi <850593913@qq.com>
2022-04-01 11:46:04 +0800

    快速排序_v1.1.0

--- 快速排序/index.js
+++ 快速排序/index.js
@@ -1,16 +1,35 @@
-function quickSort(n) {
+// 快速排序
+function quickSort(array, start = 0, end = array.length - 1) {
   // 递归出口
-  if (n.length < 2) {
-    return n;
+  if (end - start < 1) {
+    return array;
   }
+  // 分界点位置
+  let pivotIndex = partition(array, start, end);
+  // 递归处理左边比分界点小的元素
+  quickSort(array, start, pivotIndex - 1);
+  // 递归处理左边比分界点大的元素
+  quickSort(array, pivotIndex + 1, end);
+  return array;
+}
+
+function partition(array, start, end) {
+  // 需要交换的位置
+  let j = start;
   // 找到分界点元素
-  let pivot = n[n.length - 1];
-  // 拆分成左右两个数组
-  let left = n.filter((num, i) => num <= pivot && i != n.length - 1);
-  let right = n.filter((num) => num > pivot);
-  // 继续递归拆分合并
-  return [...quickSort(left), pivot, ...quickSort(right)];
-  // 或者使用concat方法拼接数组
+  let pivot = array[end];
+  for (let i = start; i <= end; i++) {
+    // 比分界点小，往数组前面进行交换
+    if (array[i] <= pivot) {
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+      j++;
+      /// 也可以使用ES6的解构赋值进行交换
+    }
+  }
+  // 分界点位置
+  return j - 1;
 }
 
 module.exports = quickSort;
